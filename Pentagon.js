c4544b9da3c491578667ab51a13fd2da1ff9b750
Tofazzal26 <afranislamabir6789@gmail.com>
2024-02-13 14:13:26 +0600

    pentagon scritp finish

---
+++ Pentagon.js
@@ -0,0 +1,17 @@
+
+
+function pentagonCalculate() {
+    const input = document.getElementById('PentagonValue');
+    const p = commonPentagon('p');
+    const b = commonPentagon('b')
+    const output = parseFloat(0.5 * p * b);
+    input.innerText = output;
+}
+
+function commonPentagon(common) {
+    const input = document.getElementById(common)
+    const value = input.value;
+    return value;
+}
+
+
