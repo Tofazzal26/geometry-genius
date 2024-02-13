2322c0f5d7774ea958b155eb6bf32d920782ec3a
Tofazzal26 <afranislamabir6789@gmail.com>
2024-02-13 14:06:09 +0600

    rhombus script style finish

---
+++ Rhombus.js
@@ -0,0 +1,19 @@
+
+
+function rhombusCalculator() {
+    const output = document.getElementById('RhombusValue');
+    const d1 = commonRhombus('d1');
+    const d2 = commonRhombus('d2');
+    const input = parseFloat(0.5 * d1 * d2);
+    output.innerText = input;
+}
+
+function commonRhombus(common) {
+    const rhombus = document.getElementById(common);
+    const value = rhombus.value;
+    return value;
+}
+
+
+
+
